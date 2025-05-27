cdd0bac810a78cc951b7e20f217a6be071436b34
PabloMediven <pmari.mediven@gmail.com>
2025-05-27 14:43:20 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-# Entrypoint para instalar dependencias y arrancar Apache
-
-echo "Instalando dependencias de GLPI..."
-
-cd /var/www/glpi
-php bin/console dependencies install || true
-
-# Volver al directorio raíz por si acaso
-cd /var/www
-
-# Iniciar Apache en primer plano
-exec apache2-foreground
+apache2-foreground
