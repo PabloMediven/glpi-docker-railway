19657a13ea5dfb85c87c130caf388c616246f732
P M <pmari.mediven@gmail.com>
2025-05-28 12:17:39 -0300

    url public

--- config_db.php
+++ config_db.php
@@ -1,6 +1,6 @@
 <?php
 class DB extends DBmysql {
-    public $dbhost     = 'mysql.railway.internal';
+    public $dbhost     = 'mysql://root:HWrMLtkDBJbAhKCFpdWnfYfHuPDwaLUY@maglev.proxy.rlwy.net:10161/railway';
     public $dbuser     = 'root';
     public $dbpassword = 'HWrMLtkDBJbAhKCFpdWnfYfHuPDwaLUY';
     public $dbdefault  = 'railway';
